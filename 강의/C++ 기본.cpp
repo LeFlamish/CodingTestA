d67714d367f6a1db600827e42bb7a2f4382090d4
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2025-03-10 00:24:04 +0900

    Create C++ 기본.cpp

---
+++ 강의/C++ 기본.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // C++에서는 stdio.h 대신 iostream 사용
+using namespace std; // cin, cout 등의 함수의 위치는 std에 있음. 매번 std::cin, std::cout으로 써주기 귀찮기 때문에 자동으로 std 채우는 코드
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0); // C의 입출력(scanf, printf)를 절대 사용 불가능하지만, 코드의 실행 속도가 빨라지므로 필수.
+	return 0;
+}
