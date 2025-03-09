f9de2a9624102919ce89ced8e46bf3ef1c0eb239
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2025-03-10 00:40:18 +0900

    Update C++ 기본.cpp

--- 강의/C++ 기본.cpp
+++ 강의/C++ 기본.cpp
@@ -1,3 +1,6 @@
+// C++로 문제를 푼다면 아래 5줄 정도는 눈 감고도 입력할 수 있도록 하자.
+// 살기 위해서는 숨을 쉬듯 너무나도 당연한 기본적인 코드이다.
+
 #include <iostream> // C++에서는 stdio.h 대신 iostream 사용
 using namespace std; // cin, cout 등의 함수의 위치는 std에 있음. 매번 std::cin, std::cout으로 써주기 귀찮기 때문에 자동으로 std 채우는 코드
 
