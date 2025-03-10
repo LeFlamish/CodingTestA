4c6c0f9ac7f9a167f466367293dac247ed0e5d63
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2025-03-10 18:49:49 +0900

    Update 1_C++ 기본.cpp

--- 강의/1_C++ 기본.cpp
+++ 강의/1_C++ 기본.cpp
@@ -4,7 +4,12 @@
 #include <iostream> // C++에서는 stdio.h 대신 iostream 사용
 using namespace std; // cin, cout 등의 함수의 위치는 std에 있음. 매번 std::cin, std::cout으로 써주기 귀찮기 때문에 자동으로 std 채우는 코드
 
+int x, y, z;
+String str; // C++은 String(문자열) 자료형이 존재한다.
+
 int main() {
 	cin.tie(0)->sync_with_stdio(0); // C의 입출력(scanf, printf)를 절대 사용 불가능하지만, 코드의 실행 속도가 빨라지므로 필수.
+	cin >> x >> y >> z >> str; // cin으로 입력을 받는다. 연속적인 입력 가능.
+	cout << x << ' ' << y << z << '\t' << str << '\n'; // cout으로 출력한다. 연속적인 출력 가능.
 	return 0;
 }
