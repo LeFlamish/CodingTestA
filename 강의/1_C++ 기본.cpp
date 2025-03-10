45d8a8298952f9d41dc09d590bd53e6e1cd9ba89
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2025-03-10 19:58:52 +0900

    Update 1_C++ 기본.cpp

--- 강의/1_C++ 기본.cpp
+++ 강의/1_C++ 기본.cpp
@@ -6,10 +6,15 @@ using namespace std; // cin, cout 등의 함수의 위치는 std에 있음. 매�
 
 int x, y, z;
 String str; // C++은 String(문자열) 자료형이 존재한다.
+auto var; // C++에서는 자동으로 자료형을 맞춰주는 auto 자료형이 존재한다.
+int arr[10];
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0); // C의 입출력(scanf, printf)를 절대 사용 불가능하지만, 코드의 실행 속도가 빨라지므로 필수.
 	cin >> x >> y >> z >> str; // cin으로 입력을 받는다. 연속적인 입력 가능.
 	cout << x << ' ' << y << z << '\t' << str << '\n'; // cout으로 출력한다. 연속적인 출력 가능.
+	for (auto i : arr) { // C++에서는 반복자를 설정해서 반복문을 돌릴 수도 있다.
+		cout << i << ' ';
+	}
 	return 0;
 }
